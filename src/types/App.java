e0424476136d9f562b9f497fbd04b31c74e0f44c
unknown <lucy.yates@ntlworld.com>
2024-01-15 21:44:49 +0000

    Working overtime

---
+++ src/types/App.java
@@ -0,0 +1,27 @@
+package types;
+
+public class App {
+
+	public static void main(String[] args) {
+		byte byt = 0125;
+		short shrt = 255;
+		int i = 2_100_000_000;
+//		Without the L Java thinks it's an int
+		long l = 99999999999999999L;
+		// Need the f or Java thinks it's a double
+		float f = 44.94F;
+		double d = 44.94;
+
+		boolean b = false;
+
+		// '' for chars and "" for strings
+		char c = 'j';
+		String s = "Jordan";
+
+		// Primitives can never be null
+		s = null;
+		char capital = 'j' - 32;
+
+		System.out.println(capital);
+	}
+}
