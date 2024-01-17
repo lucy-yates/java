0b8e661f3161f5e498da2fb6d3a428a32c4ebbf0
unknown <lucy.yates@ntlworld.com>
2024-01-17 11:13:46 +0000

    Fixed it.. again

--- src/garage/App.java
+++ src/garage/App.java
@@ -3,8 +3,7 @@ package garage;
 public class App {
 
 	public static void main(String[] args) {
-		Car kelly = new Car("Purple", "Ford", "Small");
-
+		Car kelly = new Car("Purple", "Ford", "Small", true);
 		kelly.print();
 
 		Plane vulcan = new Plane("Green", "RAF", "Big");
@@ -12,6 +11,9 @@ public class App {
 
 		Boat iona = new Boat("White", "P&O", "Massive");
 		iona.print();
+
+		Car henners = new Car("Black", "Ford", "Medium", true);
+		henners.print();
 	}
 
 }
--- src/garage/Car.java
+++ src/garage/Car.java
@@ -1,20 +1,34 @@
 package garage;
 
 public class Car extends Vehicle {
+	private boolean boot;
 
 	public Car() {
 		super();
 	}
 
-	public Car(String colour, String brand, String size) {
+	public Car(String colour, String brand, String size, boolean boot) {
 		super(colour, brand, size);
-		setColour(colour);
-		setBrand(brand);
-		setSize(size);
+		this.boot = boot;
 	}
 
 	public void vroom() {
 		System.out.println("vroom");
 	}
 
+	public boolean hasBoot() {
+		return bigboot;
+	}
+
+	public void setBoot(boolean boot) {
+		this.boot = boot;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("Colour: " + this.getColour());
+		System.out.println("Brand: " + this.getBrand());
+		System.out.println("Size: " + this.getSize());
+		System.out.println("Boot: " + this.boot);
+	}
 }
