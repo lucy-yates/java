ec4e5b4ae3c9448182f32fb4a1f20a71fe8641b0
unknown <lucy.yates@ntlworld.com>
2024-01-17 10:30:04 +0000

    Saving before I make it worse

--- src/garage/Car.java
+++ src/garage/Car.java
@@ -3,9 +3,11 @@ package garage;
 public class Car extends Vehicle {
 
 	public Car() {
+		super();
 	}
 
 	public Car(String colour, String brand, String size) {
+		super(colour, brand, size);
 		setColour(colour);
 		setBrand(brand);
 		setSize(size);
